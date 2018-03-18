ff15fa8a10b1d456f90fdef967e2c8cd1274873f
Taylor Ward <taylortheward@gmail.com>
2018-03-18 12:48:12 -0700

    Add console.logs)

--- Controllers/home_controller.js
+++ Controllers/home_controller.js
@@ -7,9 +7,8 @@ const LATEST_COUNT = 3;
 exports.getFeatured = (req, res) => {
 	let id = req.query.id;
 
-	console.log(process.env.NODE_ENV);
-	
 	if (process.env.NODE_ENV === 'production') {
+		console.log('we in the production environment');
 		id = '5a8329ae38e29400149dd1d5';
 	}
 
@@ -18,6 +17,7 @@ exports.getFeatured = (req, res) => {
 			if (c) {
 				res.json(c);
 			} else {
+				console.log('failed to find featured connection');
 				// Failed to find original connection so return default
 				Connection.findOne()
 					.then(c => {
